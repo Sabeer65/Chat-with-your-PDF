cdea4bd42d1c97c7f0fef77f26b0f8b7a32555df
Sabeer <sabeer.h4774@gmail.com>
2026-01-31 17:08:01 +0530

    Added chat history and memory logic

--- app.py
+++ app.py
@@ -10,71 +10,91 @@ st.title("Chat with your PDF")
 
 llm = ChatOllama(model="llama3.2", temperature=0)
 
-pdffile = st.file_uploader(label="Upload your pdf", accept_multiple_files=False, type=["pdf"])
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
 if "vector_store" not in st.session_state:
     st.session_state.vector_store = None
 
+with st.sidebar:
 
-if pdffile is not None:
-    try:
-        
-        # checking if already processed to save time 
+    pdffile = st.file_uploader(label="Upload your pdf", accept_multiple_files=False, type=["pdf"])
+    
+    if pdffile is not None:
         if st.session_state.vector_store is None:
+            try:
+                reader = PdfReader(pdffile)
+                pdf_text = ''
 
-            reader = PdfReader(pdffile)
-            pdf_text = ''
+                with st.spinner("Wait for it...", show_time=True):
+                    for pages in reader.pages:
+                        pdf_text += pages.extract_text()
 
-            with st.spinner("Wait for it...", show_time=True):
-                for pages in reader.pages:
-                    pdf_text += pages.extract_text()
+                    # Splitting the text into chunks
+                    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+                    chunks = text_splitter.split_text(pdf_text)
+                    
+                    # embedding the text and storing it in session state
+                    embeddings = OllamaEmbeddings(model="llama3.2")
+                    st.session_state.vector_store = FAISS.from_texts(chunks, embeddings)
 
-                # Splitting the text into chunks
-                text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-                chunks = text_splitter.split_text(pdf_text)
-                
-                # embedding the text and storing it in session state
-                embeddings = OllamaEmbeddings(model="llama3.2")
-                st.session_state.vector_store = FAISS.from_texts(chunks, embeddings)
-
-            st.success("Pdf Loaded succesfully")
+                st.success("Pdf Loaded succesfully")
 
 
-    except Exception as exc:
-        st.error(f"Failed to load PDF {exc}")
+            except Exception as exc:
+                st.error(f"Failed to load PDF {exc}")
 
-    user_input = st.chat_input("Type your question here")
+for msg in st.session_state.messages:
+    with st.chat_message(msg['role']):
+        st.write(msg['content'])
 
-    # If no user input or vector_store do not run code
-    if user_input and st.session_state.vector_store is not None:
+user_input = st.chat_input("Type your question here")
 
-        st.chat_message("user").write(user_input)
+# If no user input or vector_store do not run code
+if user_input and st.session_state.vector_store is not None:
+
+    st.session_state.messages.append({'role':'user', 'content':user_input})
+
+    with st.chat_message("user"):
+        st.write(user_input)
+
+    with st.spinner("Thinking..."):
+        # Search for the user input in the vector store
+        docs = st.session_state.vector_store.similarity_search(user_input)
+
+        if docs:
+            context = ''
+            for doc in docs:
+                context += doc.page_content +"\n\n" # Set the context from the docs
+            history = ''
+            for msg in st.session_state.messages:
+                history += f"{msg['role']}: {msg['content']}\n"
+            # Prompt for the llm 
+            prompt = f"""
+        You are a helpful AI assistant. 
+
+        --- CHAT HISTORY ---
+            {history}
+            --------------------
+
+        The user has uploaded a PDF document, and here is the relevant content extracted from it:
         
-        with st.spinner("Thinking..."):
-            # Search for the user input in the vector store
-            docs = st.session_state.vector_store.similarity_search(user_input)
+        --- PDF CONTENT START ---
+        {context}
+        --- PDF CONTENT END ---
+        
+        User Question: {user_input}
+        
+        Answer (based ONLY on the PDF content above):
+        """
+            
+            response = llm.invoke(prompt)
 
-            if docs:
-                context = ''
-                for doc in docs:
-                    context += doc.page_content +"\n\n" # Set the context from the docs
-                
-                # Prompt for the llm 
-                prompt = f"""
-            You are a helpful AI assistant. 
-            The user has uploaded a PDF document, and here is the relevant content extracted from it:
-            
-            --- PDF CONTENT START ---
-            {context}
-            --- PDF CONTENT END ---
-            
-            User Question: {user_input}
-            
-            Answer (based ONLY on the PDF content above):
-            """
-                
-                response = llm.invoke(prompt)
-                st.chat_message("assistant").write(response.content)
+            st.session_state.messages.append({'role':'assistant', 'content':response.content})
 
-            else:
-                st.warning("i couldnt find any relevant info in the PDF")
+            with st.chat_message("assistant"):
+                st.write(response.content)
+
+        else:
+            st.warning("i couldnt find any relevant info in the PDF")
